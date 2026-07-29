71a9994a36410229c84160ad49f2df840e8fa6e5
yu2b <biubiuyu@gmail.com>
2026-07-29 16:24:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 d3.csv("data/housing_bayarea_2010_2025.csv")
   .then(data => {
+    data.forEach(row => {
+      row.Date = d3.timeParse("%Y-%m-%d")(row.Date);
+      row.ZHVI = +row.ZHVI;
+    });
+    const santaClaraData = data.filter( row => {
+      return row.County == "Santa Clara County";
+    });
     console.log(data);
-  });
+});

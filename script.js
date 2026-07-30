cea1b05fa0aa8528f0f6c78e94b9ce925ddee9d4
yu2b <biubiuyu@gmail.com>
2026-07-29 17:10:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,45 @@ d3.csv("data/housing_bayarea_2010_2025.csv")
       row.Date = d3.timeParse("%Y-%m-%d")(row.Date);
       row.ZHVI = +row.ZHVI;
     });
-    const santaClaraData = data.filter( row => {
-      return row.County == "Santa Clara County";
+
+    const santaClaraData = data.filter(row => {
+      return row.County === "Santa Clara County";
     });
+
     console.log(data);
     console.log(santaClaraData);
 
-});
+    const width = 800;
+    const height = 500;
+    const margin = {
+      top: 40,
+      right: 30,
+      bottom: 50,
+      left: 80  
+    };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const svg = d3.select("#chart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    const chart = svg.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(santaClaraData, row => row.Date))
+      .range([0, innerWidth]);
+    
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(santaClaraData, row => row.ZHVI))
+      .range([innerHeight, 0]);
+    
+    chart.append("g")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(d3.axisBottom(xScale));
+
+    chart.append("g")
+      .call(d3.axisLeft(yScale));
+    
+  });
